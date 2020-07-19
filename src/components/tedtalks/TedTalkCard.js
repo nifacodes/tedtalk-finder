a070a96a493f9b1ba877e94ba8753059c1eafd7b
nifacodes <nifacodes@gmail.com>
2020-07-19 16:57:53 -0400

    deploy on netlify

--- src/components/tedtalks/TedTalkCard.js
+++ src/components/tedtalks/TedTalkCard.js
@@ -28,7 +28,7 @@ const TedTalkCard = ({ tedTalkItem: { name, speaker, viewCount, youTubeID, speak
         <>
             <Col xs={12} md={3}>
                 <Card style={{ borderRadius: '5px', backgroundColor: '#211a23', color: '#faf0dc' }} >
-                    {thumbnailsById[youTubeID] ? <Card.Img className="" variant="top" src={thumbnailsById[youTubeID]} style={{ borderRadius: '5px', height: '100%', width: '100%' }} /> : <Card.Header><Container><Spinner spinnerStyle={spinnerStyle} /> </Container></Card.Header>}
+                    {thumbnailsById[youTubeID] ? <Card.Img className="" variant="top" src="https://i.ytimg.com/vi/J-I2eScPkEw/maxresdefault.jpg" style={{ borderRadius: '5px', height: '100%', width: '100%' }} /> : <Card.Header><Container><Spinner spinnerStyle={spinnerStyle} /> </Container></Card.Header>}
                     <Card.Body>
                         <Card.Title>{withoutNameStr}</Card.Title>
                         <Card.Text>
